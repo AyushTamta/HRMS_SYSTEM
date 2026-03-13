02f75f96b0d45787e3b4395663ba936824e83fd5
Ayush Tamta <42358514+AyushTamta@users.noreply.github.com>
2026-03-13 18:33:37 +0530

    Update App.jsx

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const API = "https://hrms-system-ayush.onrender.com/api";
 
 function App() {
   const [employees, setEmployees] = useState([]);
